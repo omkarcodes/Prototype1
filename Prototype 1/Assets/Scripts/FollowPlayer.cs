1decbcbbb8b1ea2c1e9ec18071b5c6e5039d7d01
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-02-10 16:19:58 +0530

    Added Project

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //Variables
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -8.44f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // LateUpdate is called after the update method and is used to prevent delay
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Here are Variables
+    private float speed = 30f;
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+    private float forwardInput;
+
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Getting Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
+        
+        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
+    }
+}
